1f7d7fffbb4ed9b1e3ee5bb05eda60a178d8ed49
juanortiz <juanmoo@me.com>
2016-08-11 14:03:06 -0400

    Added Test classes
    These classes can be used to check that the mechanism of the lights and the buzzer reader function properly.

---
+++ Raspi/Python/TestFiles/Arduino/BuzzState.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+import smbus
+import time
+
+#Initialize IIC buzzState
+bus = smbus.SMBus(1)
+arduino_address = 0x10
+
+while (True):
+    time.sleep(.5)
+    data = bus.read_i2c_block_data(arduino_address,0)
+    data2 = []
+    for x in range(8):
+        data2.append(data[x])
+    print data2
